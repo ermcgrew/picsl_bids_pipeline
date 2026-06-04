d711376027d00078e35862cc92a2c2485a364b63
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2026-06-04 13:00:41 -0400

    superres with tmpdir remove in trap

--- bids_superres.sh
+++ bids_superres.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
 
+set -x 
+
 module load c3d
 module load python
+
+SRPATH="/project/wolk_4/naccsc_bids/scripts/adni_processing_pipeline/utilities/build_release"
+echo Script versions: 
+echo $SRPATH
+which c3d
+which python
 
 # inputs
 input_file=$1
@@ -11,41 +19,36 @@ output_directory=$2
 sr_bids_entity='{"desc":"superres"}'
 output_file_name=$( python /project/wolk_4/naccsc_bids/scripts/picsl_bids_pipeline/bids_file_names.py -i $(basename $input_file) -o $sr_bids_entity)
 final_sr_output_filepath="${output_directory}/${output_file_name}"
-echo 
-echo
-# echo $output_file_name
-echo $final_sr_output_filepath
+echo Output image will be stored as: $final_sr_output_filepath
 
-## use tmp dir for intermediate files, then only copy over final file (check with Sandy that the intermediate files aren't needed)
 ## make this run script part of SRpath package? 
-# make singularity container from SRPATH -- Long & Paul were the ones who wrote it? 
+## make singularity container from SRPATH -- Long & Paul were the ones who wrote it? 
 
-SRPATH="/project/wolk_4/naccsc_bids/scripts/adni_processing_pipeline/utilities/build_release"
-echo Script versions: 
-echo $SRPATH
-which c3d
-which python
+## Run in tmpdir, always remove tmpdir when script exits
+tmpdir=$( mktemp -d --tmpdir=/scratch )
+echo Running in $tmpdir
+trap "rm -rf $tmpdir" EXIT
 
-# $SRPATH/NLMDenoise \
-#     -i $input_file \
-#     -o $output_directory/T1w_trim_denoised.nii.gz
+$SRPATH/NLMDenoise \
+    -i $input_file \
+    -o $tmpdir/T1w_trim_denoised.nii.gz
 
-# c3d $output_directory/T1w_trim_denoised.nii.gz \
-#     -swapdim RPI \
-#     -o $output_directory/T1w_trim_denoised.nii.gz
+c3d $tmpdir/T1w_trim_denoised.nii.gz \
+    -swapdim RPI \
+    -o $tmpdir/T1w_trim_denoised.nii.gz
 
-# $SRPATH/NLMUpsample \
-#     -i $output_directory/T1w_trim_denoised.nii.gz \
-#     -o $output_directory/T1w_trim_denoised_SR.nii.gz \
-#     -lf 2 1 2
+$SRPATH/NLMUpsample \
+    -i $tmpdir/T1w_trim_denoised.nii.gz \
+    -o $tmpdir/T1w_trim_denoised_SR.nii.gz \
+    -lf 2 1 2
 
-# orient_code=$(c3d $input_file -info | cut -d ';' -f 5 | cut -d ' ' -f 5)
-# if [[ $orient_code == "Oblique," ]]; then
-#     orient_code=$(c3d $input_file -info | cut -d ';' -f 5 | cut -d ' ' -f 8)
-# fi
+orient_code=$(c3d $input_file -info | cut -d ';' -f 5 | cut -d ' ' -f 5)
+if [[ $orient_code == "Oblique," ]]; then
+    orient_code=$(c3d $input_file -info | cut -d ';' -f 5 | cut -d ' ' -f 8)
+fi
 
-# c3d $output_directory/T1w_trim_denoised_SR.nii.gz\
-#     -swapdim $orient_code \
-#     -clip 0 inf \
-#     -type short \
-#     -o $final_sr_output_file
+c3d $tmpdir/T1w_trim_denoised_SR.nii.gz\
+    -swapdim $orient_code \
+    -clip 0 inf \
+    -type short \
+    -o $final_sr_output_filepath
